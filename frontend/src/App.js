adc8a94c2ff0a4005acc5fc4d2e118d0e210e65a
mxrah10 <muhtasim.rahman21@gmail.com>
2023-07-10 17:37:00 -0700

    Added some visual changes to the UI and thinking of adding an authentication system for users which is why i am adding a user image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,11 +8,19 @@ import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <Router>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand as={Link} to="/">RecommendIO</Navbar.Brand>
+      <Navbar bg="dark" variant="dark" style={{ paddingTop: '20px', paddingBottom: '20px' }}>
+        <Navbar.Brand as={Link} to="/" style={{ paddingLeft: '15px' }}>RecommendIO</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link as={Link} to="/">Home</Nav.Link>
           <Nav.Link as={Link} to="/about">About</Nav.Link>
+          <Nav.Link as={Link} to="/anime">Anime</Nav.Link>
+          <Nav.Link as={Link} to="/movies">Movies</Nav.Link>
+          <Nav.Link as={Link} to="/games">Games</Nav.Link>
+        </Nav>
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/user">
+            <img src="https://i.imgur.com/1uYrojn.png" alt="user" width="30" height="30" />
+          </Nav.Link>
         </Nav>
       </Navbar>
       <Routes>
