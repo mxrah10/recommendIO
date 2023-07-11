bfe8f378d09150cb189fcc358a5c07d08dcd7207
mxrah10 <muhtasim.rahman21@gmail.com>
2023-07-10 17:11:34 -0700

    new react modules installed and added background and some design to the pages, fixed react-route-dom Switch module deprecation because Routes does the same job and will be relevant going forward

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 
@@ -17,10 +17,10 @@ function App() {
             </li>
           </ul>
         </nav>
-        <Switch>
+        <Routes>
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
-        </Switch>
+        </Routes>
       </div>
     </Router>
   );
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Container } from 'react-bootstrap';
 
 const Home = () => (
-  <div>
-    <h2>Home Page</h2>
-    <Link to="/anime/1">Test Anime</Link>
-    <br/>
-    <Link to="/about">About Page</Link>
-  </div>
+  <Container className="center-content text-center">
+    <h1 className="mb-4">Welcome to AnimeWorld!</h1>
+    <p className="mb-4">Your one-stop platform for all things anime.</p>
+    <Button variant="light" as={Link} to="/anime/1" className="mr-2">Go to Test Anime</Button>
+    <Button variant="light" as={Link} to="/about">Go to About Page</Button>
+  </Container>
 );
 
 export default Home;
