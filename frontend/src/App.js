a8f8f80723b09b5654493ca1d12bd6d1cdf9884d
mxrah10 <muhtasim.rahman21@gmail.com>
2023-07-12 15:15:54 -0700

    made some changes to the frontend UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <Router>
-      <Navbar bg="dark" variant="dark" className="justify-content-between" style={{ paddingTop: '20px', paddingBottom: '20px' }}>
+      <Navbar bg="dark" variant="dark" className="justify-content-between fixed-navbar" style={{ paddingTop: '20px', paddingBottom: '20px' }}>
         <Navbar.Brand as={Link} to="/" style={{ paddingLeft: '15px', fontSize: '25px' }}>RecommendIO</Navbar.Brand>
-        <Nav className="mr-auto">
+        <Nav className="mr-auto" style={{paddingRight:'50px'}}>
           <Nav.Link as={Link} to="/">Home</Nav.Link>
           <Nav.Link as={Link} to="/about">About</Nav.Link>
           <Nav.Link as={Link} to="/anime">Anime</Nav.Link>
@@ -18,7 +18,7 @@ function App() {
           <Nav.Link as={Link} to="/games">Games</Nav.Link>
         </Nav>
         <Nav>
-          <Nav.Link as={Link} to="/user" style={{paddingRight: '140px'}}>
+          <Nav.Link as={Link} to="/user" style={{paddingRight: '100px'}}>
             <img src="https://imageio.forbes.com/blogs-images/olliebarder/files/2016/06/goku_black_new-1200x675.jpg?height=399&width=711&fit=bounds" alt="user" style={{ width: "50px", height: "50px", objectFit: "cover" , borderRadius: "50%" }} />
           </Nav.Link>
         </Nav>
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Container } from 'react-bootstrap';
+import './App.css';
 
-const Home = () => (
-  <Container className="center-content text-center">
-    <h1 className="mb-4">Welcome to RecommendIO!</h1>
-    <p className="mb-4">Your one-stop platform for all things anime, movies and games.</p>
-    <Button variant="light" as={Link} to="/anime/1" className="mr-2">Go to Test Anime</Button>
-    <Button variant="light" as={Link} to="/movies/1" className="mr-2">Go to Test Movies</Button>
-    <Button variant="light" as={Link} to="/games/1" className="mr-2">Go to Test Games</Button>
-    <Button variant="light" as={Link} to="/about">Go to About Page</Button>
-  </Container>
-);
+function Home() {
+  return (
+    <Container className="center-content text-center">
+      <h1 className="mb-4">Welcome to RecommendIO!</h1>
+      <p className="mb-4">Your one-stop platform for all things anime, movies and games.</p>
+
+      <Button variant="dark" as={Link} to="/anime/1" className="mr-3 my-2 equal-width-buttons">Go to Test Anime</Button>
+      <Button variant="dark" as={Link} to="/movies/1" className="mr-3 my-2 equal-width-buttons">Go to Test Movies</Button>
+      <Button variant="dark" as={Link} to="/games/1" className="mr-3 my-2 equal-width-buttons">Go to Test Games</Button>
+      <Button variant="dark" as={Link} to="/about" className="mr-3 my-2 equal-width-buttons">Go to About Page</Button>
+    </Container>
+  );
+}
 
 export default Home;
