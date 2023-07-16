08f385460abfcdf2268390c814de773dfdc64ae9
mxrah10 <muhtasim.rahman21@gmail.com>
2023-07-15 22:13:33 -0700

    found a better framework called bootstrap which is suppposed to have everything in it and i just use certain keywords to style instead of doing css

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -8,7 +8,6 @@ function Home() {
     <Container className="center-content text-center">
       <h1 className="mb-4">Welcome to RecommendIO!</h1>
       <p className="mb-4">Your one-stop platform for all things anime, movies and games.</p>
-
       <Button variant="dark" as={Link} to="/anime/1" className="mr-3 my-2 equal-width-buttons">Go to Test Anime</Button>
       <Button variant="dark" as={Link} to="/movies/1" className="mr-3 my-2 equal-width-buttons">Go to Test Movies</Button>
       <Button variant="dark" as={Link} to="/games/1" className="mr-3 my-2 equal-width-buttons">Go to Test Games</Button>
