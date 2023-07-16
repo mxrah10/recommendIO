9458adcdf4c81740e2e3d817820861869939c788
mxrah10 <muhtasim.rahman21@gmail.com>
2023-07-15 21:58:51 -0700

    added a scss extension and scss template because im tired ot styling and css, so most of it should be auto from now

--- frontend/src/Anime.js
+++ frontend/src/Anime.js
@@ -1,10 +1,28 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 
+function SearchBar() {
+    return (
+        <main>
+            <div className="main-head">
+                <form 
+                    className="search-box">
+                    <input 
+                        type="search"
+                        placeholder="Search for an anime..."
+                        required
+                        />
+                </form>
+            </div>
+        </main>
+    );
+}
+
 const Anime = () => {
     return (
         <Container className="center-content">
             <h1>Test Anime</h1>
+            <SearchBar />
             <p>
                 This is a test page for anime.
             </p>
